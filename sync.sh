2184767eeb4d1e4a563bdccbe442322c2a520c68
Mike Del Balso <mdb@uber.com>
2018-04-10 14:46:35 -0700

    got composoite video working

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ma codesync --last /Users/delbalso/projects/dream/src -ig *.pb *.mat
+#ma codesync --last /Users/delbalso/projects/dream/src --ignore="*.zip" --ignore="*.pb" --ignore="*.mat" --ignore="*.zip"
+ma codesync --last /Users/delbalso/projects/dream/src --ignore "*.zip" --ignore "*.mat"
